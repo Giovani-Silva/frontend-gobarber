bb9055d7b7127ddd4da5bc5a6612134f41eedf2a
unknown <giovani.silva@live.com>
2020-11-04 18:19:29 -0300

    Estrutura de toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
+
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 import GlobalStyle from './styles/global';
@@ -9,6 +11,7 @@ import GlobalStyle from './styles/global';
 const App: React.FC = () => (
   <>
     <GlobalStyle />
+    <ToastContainer />
     <AuthProvider>
       <Router>
         <Routes />
